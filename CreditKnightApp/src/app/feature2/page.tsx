c1149b9435661d25b3d639ec6a1e243f62ef3f47
Diego Mendoza <sapito12@yahoo.com>
2024-10-05 14:39:03 -0400

    Added button features; Colors, Hover Effect, and Links

--- CreditKnightApp/src/app/feature2/page.tsx
+++ CreditKnightApp/src/app/feature2/page.tsx
@@ -1,37 +1,45 @@
 import React from 'react';
 import Link from 'next/link';
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-export default async function Feature() {
 
-    const {getUser } = getKindeServerSession();
-    const user = await getUser();
-    console.log(user);
-    return (
-      <div className="container">
-        {/* Navigation Buttons */}
-      <div style={{ textAlign: 'center', marginBottom: '20px' }}>
-        <Link href="/feature1">
-          <button style={{ margin: '0 10px' }}>Feature 1</button>
-        </Link>
-        <Link href="/feature2">
-          <button style={{ margin: '0 10px' }}>Feature 2</button>
-        </Link>
-        <Link href="/feature3">
-          <button style={{ margin: '0 10px' }}>Feature 3</button>
-        </Link>
-      </div>
-        <div className="card hero">
-          <h1>This is the page for feature 2.</h1>
-          <p>Currently a work in progress. Apologies for the inconvenience.</p>
-          
-          {/* Smaller Box */}
-          <div className="small-box" style = {{ backgroundColor: '#b8860b', color: 'white'}}>
-            <p>Transaction??? maybe!1!?(example)</p>
-          </div>
+const features = [
+  { href: '/feature1', label: 'Feature 1' },
+  { href: '/feature2', label: 'Feature 2' },
+  { href: '/feature3', label: 'Feature 3' },
+];
+
+const renderNavButtons = () => (
+  <div className="nav-buttons-container">
+    {features.map((feature) => (
+      <Link key={feature.href} href={feature.href} legacyBehavior>
+        <button className="nav-button" >{feature.label}</button>
+      </Link>
+    ))}
+  </div>
+);
+
+export default async function Feature() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  console.log(user);
+
+  return (
+    <div className="container">
+      {/* Navigation Buttons */}
+      {renderNavButtons()}
+
+      <div className="card hero">
+        <h1>This is the page for feature 2.</h1>
+        <p>Currently a work in progress. Apologies for the inconvenience.</p>
+        
+        {/* Smaller Box */}
+        <div className="small-box"  style={{ backgroundColor: '#b8860b', color: 'white' }}>
+          <p>scam!!!!!!!!! (example)</p>
         </div>
-        <section className="next-steps-section">
-          <h2 className="text-heading-1">Next steps for you</h2>
-        </section>
       </div>
-    );
-  }
+      <section className="next-steps-section">
+        <h2 className="text-heading-1">Next steps for you</h2>
+      </section>
+    </div>
+  );
+}
