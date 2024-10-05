602cdb8eecfdb163eb5691a877033bdf57b87805
Yoendry <yoendryferro2003@gmail.com>
2024-10-05 17:20:20 -0400

    delete files in locale and the image file

--- CreditKnightApp/src/app/layout.tsx
+++ CreditKnightApp/src/app/layout.tsx
@@ -26,7 +26,6 @@ export default async function RootLayout({
         <header>
           <nav className="nav container">
             <h1 className="text-display-3">CreditKnight</h1>
-            <img src="/shield-svgrepo-com.svg" alt="shield Icon" style={{ width: '24px', height: '24px', marginRight: '8px' }} />
             <div>
               {!(await isAuthenticated()) ? (
                 <>
