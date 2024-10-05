eec692a84fd1f934499a38d219afcd56f5ba5711
Diego Mendoza <sapito12@yahoo.com>
2024-10-05 16:39:55 -0400

    Added feature 4 buttons

--- CreditKnightApp/src/app/feature4/page.tsx
+++ CreditKnightApp/src/app/feature4/page.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
+import Link from 'next/link';
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-export default async function Feature() {
 
-    const {getUser } = getKindeServerSession();
-    const user = await getUser();
-    console.log(user);
-    return (
-      <div className="container">
-        <div className="card hero">
-          <h1>This is the page for feature 4.</h1>
-          <p>Currently a work in progress. Apologies for the inconvenience.</p>
-          
-          {/* Smaller Box */}
-          <div className="small-box" style = {{ backgroundColor: '#00ff00', color: '#ffffff'}}>
-            <p>unknown territory...(example)</p>
-          </div>
+const features = [
+  { href: '/dashboard', label: 'Dashboard' },
+  { href: '/feature1', label: 'Feature 1' },
+  { href: '/feature2', label: 'Feature 2' },
+  { href: '/feature3', label: 'Feature 3' },
+  { href: '/feature4', label: 'Feature 4' },
+];
+
+const renderNavButtons = () => (
+  <div className="nav-buttons-container">
+    {features.map((feature) => (
+      <Link key={feature.href} href={feature.href} legacyBehavior>
+        <button className="nav-button">{feature.label}</button>
+      </Link>
+    ))}
+  </div>
+);
+
+export default async function Feature() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  console.log(user);
+  
+  return (
+    <div className="container">
+      {/* Navigation Buttons */}
+      {renderNavButtons()}
+
+      <div className="card hero">
+        <h1>FAQ</h1>
+        <p>Currently a work in progress. Apologies for the inconvenience.</p>
+        
+        {/* Smaller Box */}
+        <div className="small-box"  style={{ backgroundColor: '#b8860b', color: 'white' }}>
+          <p>your questions will be answered! ^_^  (example)</p>
         </div>
-        <section className="next-steps-section">
-          <h2 className="text-heading-1">Next steps for you</h2>
-        </section>
       </div>
-    );
-  }
+      <section className="next-steps-section">
+        <h2 className="text-heading-1">Next steps for you</h2>
+      </section>
+    </div>
+  );
+}
